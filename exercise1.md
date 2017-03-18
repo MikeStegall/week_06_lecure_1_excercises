9c7933f2853835ee9bc1ae7dc09275714b56c469
MikeStegall <mestegall5@gmail.com>
2017-03-17 22:31:10 -0500

    Finish the second exercise by using video game characters and marvel characters.

---
+++ exercise1.md
@@ -0,0 +1,2 @@
+## In your own words, explain how object literals are used to store related information and why that is useful.
+Object literals are very useful to hold data from many different sources. Like first name, last name, gender, race, etc... This way everything you need for one object is in one place.
---
+++ js/exercise2.js
@@ -0,0 +1,16 @@
+// Write a short program in which you create two objects whose properties describe fictional characters. Output both objects to the console.
+
+var fictional1 = {
+  fName: 'John',
+  Lname: 'Unknow',
+  origin: 'Halo'
+}
+
+var fictional2 = {
+  fName: 'Danny',
+  lName: 'Rand',
+  origin: 'Iron Fist'
+}
+
+console.log(fictional1)
+console.log(fictional2)
